7e272ceba4fb47f744ad65413c410abd56ae60bb
Simon Coggins <simon.coggins@totaralms.com>
2014-04-07 21:28:08 +1200

    Add unit tests to tree lib

--- tests/treelib_test.php
+++ tests/treelib_test.php
@@ -89,29 +89,99 @@ class tool_capexplorer_treelib_testcase extends advanced_testcase {
     }
 
     public function test_get_course_nodes() {
+        $this->resetAfterTest();
+
+        $cat = $this->getDataGenerator()->create_category();
+        $cat2 = $this->getDataGenerator()->create_category();
+        $course1 = $this->getDataGenerator()->create_course(array('category' => $cat->id));
+        $course2 = $this->getDataGenerator()->create_course(array('category' => $cat->id));
+        $course3 = $this->getDataGenerator()->create_course(array('category' => $cat2->id));
+
+        // Should only count courses in the selected category.
+        $courses = tool_capexplorer_get_course_nodes($cat->id);
+        $this->assertCount(2, $courses);
+
+        // Check the structure of the first item.
+        $this->check_node_structure(current($courses), 'course');
     }
 
     public function test_get_category_nodes() {
+        $this->resetAfterTest();
+
+        $cat = $this->getDataGenerator()->create_category();
+        $subcat1 = $this->getDataGenerator()->create_category(array('parent' => $cat->id));
+        $subcat2 = $this->getDataGenerator()->create_category(array('parent' => $cat->id));
+        $cat2 = $this->getDataGenerator()->create_category();
+        $subcat3 = $this->getDataGenerator()->create_category(array('parent' => $cat2->id));
+        // Should only count categories in the selected category.
+        $categories = tool_capexplorer_get_category_nodes($cat->id);
+        $this->assertCount(2, $categories);
+
+        // Check the structure of the first item.
+        $this->check_node_structure(current($categories), 'category');
     }
 
     public function test_get_block_nodes() {
+        $this->resetAfterTest();
+
+        // Blocks can be children of different contexts.
+        $cat = $this->getDataGenerator()->create_category();
+        $categorycontext = context_coursecat::instance($cat->id);
+        $course = $this->getDataGenerator()->create_course(array('category' => $cat->id));
+        $coursecontext = context_course::instance($course->id);
+
+        $blocksbefore = tool_capexplorer_get_block_nodes('course', $course->id);
+        $courseblock = $this->getDataGenerator()->create_block('online_users', array('parentcontextid' => $coursecontext->id));
+        $blocksafter = tool_capexplorer_get_block_nodes('course', $course->id);
+
+        $blocksdiff = array_diff(array_map('serialize', $blocksafter), array_map('serialize', $blocksbefore));
+        $this->assertCount(1, $blocksdiff);
+        $this->check_node_structure(unserialize(current($blocksdiff)), 'block');
+
+        $blocksbefore = tool_capexplorer_get_block_nodes('category', $cat->id);
+        $catblock = $this->getDataGenerator()->create_block('online_users', array('parentcontextid' => $categorycontext->id));
+        $blocksafter = tool_capexplorer_get_block_nodes('category', $cat->id);
+
+        $blocksdiff = array_diff(array_map('serialize', $blocksafter), array_map('serialize', $blocksbefore));
+        $this->assertCount(1, $blocksdiff);
+        $this->check_node_structure(unserialize(current($blocksdiff)), 'block');
+
     }
 
     public function test_get_system_node() {
+        $systemnode = tool_capexplorer_get_system_node();
+
+        $this->assertCount(1, $systemnode);
+        $this->check_node_structure(current($systemnode), 'system');
     }
 
     public function test_get_frontpage_node() {
+        $frontpagenode = tool_capexplorer_get_frontpage_node();
+
+        $this->assertCount(1, $frontpagenode);
+        $this->check_node_structure(current($frontpagenode), 'course');
     }
 
     public function test_get_userdir_node() {
+        $userdirnode = tool_capexplorer_get_userdir_node();
+
+        $this->assertCount(1, $userdirnode);
+        $this->check_node_structure(current($userdirnode), 'userdir');
     }
 
     public function test_get_js_tree_node() {
-    }
+        $nodeobject = new stdClass();
+        $nodeobject->name = 'Node name';
+        $nodeobject->id = 123;
+        $nodeobject->contextid = 456;
 
-    public function test_get_child_nodes() {
-    }
+        $jsnode = tool_capexplorer_get_js_tree_node($nodeobject, 'course');
 
-    public function test_get_selected_subtree() {
+        $this->assertObjectHasAttribute('canHaveChildren', $jsnode);
+        $this->assertObjectHasAttribute('label', $jsnode);
+
+        $this->assertEquals(123, $jsnode->data->instanceId);
+        $this->assertEquals(456, $jsnode->data->contextId);
+        $this->assertEquals('course', $jsnode->data->nodeType);
     }
 }
